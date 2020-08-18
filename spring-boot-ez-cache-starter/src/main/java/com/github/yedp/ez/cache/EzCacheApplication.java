7e6cdcde0ff16026a9ee43be5177474781f415e2
daoping.ye <daoping.ye@amh-group.com>
2020-08-18 10:34:00 +0800

    init

---
+++ spring-boot-ez-cache-starter/src/main/java/com/github/yedp/ez/cache/EzCacheApplication.java
@@ -0,0 +1,15 @@
+package com.github.yedp.ez.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class EzCacheApplication {
+
+	public static void main(String[] args) {
+		run(EzCacheApplication.class, args);
+	}
+
+}
